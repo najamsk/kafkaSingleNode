88a06b27a841eb7ac613334fe17a61b1ffb79640
najam awan <najamsk@gmail.com>
2022-12-26 22:50:26 +0200

    confluent consumer and producer added

---
+++ cmd/confluent/consumer/go.mod
@@ -0,0 +1,5 @@
+module kconf
+
+go 1.19
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
---
+++ cmd/confluent/producer/producer.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	fmt.Println("starting confluent producer")
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	topic := "orders"
+	for _, word := range []string{"Welcome to the future", "Back to the future", "Titanic"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+}
